3b8b9acf333e62193ac281dcb8edbf047585406d
Rahul1337-spec <rahulmeshram@digitowebservices.com>
2019-09-12 10:27:05 +0530

    Realestate Version 3

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,66 +1,54 @@
-<footer class="mainfooter" role="contentinfo">
-  <div class="footer-middle">
-      <div class="container">
-        <div class="row">
-          <div class="col-md-3 col-sm-6">
-            <!--Column1-->
-            <div class="footer-pad">
-              <h4>Heading 1</h4>
-              <ul class="list-unstyled">
-                <li><a href="#"></a></li>
-                <li><a href="#">Payment Center</a></li>
-                <li><a href="#">Contact Directory</a></li>
-                <li><a href="#">Forms</a></li>
-                <li><a href="#">News and Updates</a></li>
-                <li><a href="#">FAQs</a></li>
-            </ul>
-        </div>
+<footer class=" site-footer" role="contentinfo">
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-12 col-md-6">
+        <h6>About</h6>
+        <p class="text-justify"><strong>Realestate.com</strong> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+            tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+            quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+            consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+            cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
     </div>
-    <div class="col-md-3 col-sm-6">
-        <!--Column1-->
-        <div class="footer-pad">
-          <h4>Heading 2</h4>
-          <ul class="list-unstyled">
-            <li><a href="#">Website Tutorial</a></li>
-            <li><a href="#">Accessibility</a></li>
-            <li><a href="#">Disclaimer</a></li>
-            <li><a href="#">Privacy Policy</a></li>
-            <li><a href="#">FAQs</a></li>
-            <li><a href="#">Webmaster</a></li>
+
+    <div class="col-xs-6 col-md-3">
+        <h6>Categories</h6>
+        <ul class="footer-links">
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Income Tax Form</a></li>
+            <li><a href="#">Form 16 Error</a></li>
+            <li><a href="#">Blogs</a></li>
         </ul>
     </div>
+
+    <div class="col-xs-6 col-md-3">
+        <h6>Quick Links</h6>
+        <ul class="footer-links">
+          <li><a href="#">About Us</a></li>
+          <li><a href="#">Contact Us</a></li>
+          <li><a href="#">Privacy Policy</a></li>
+          <li><a href="#">Sitemap</a></li>
+      </ul>
+  </div>
 </div>
-<div class="col-md-3 col-sm-6">
-    <!--Column1-->
-    <div class="footer-pad">
-      <h4>Heading 3</h4>
-      <ul class="list-unstyled">
-        <li><a href="#">Parks and Recreation</a></li>
-        <li><a href="#">Public Works</a></li>
-        <li><a href="#">Police Department</a></li>
-        <li><a href="#">Fire</a></li>
-        <li><a href="#">Mayor and City Council</a></li>
-        <li>
-          <a href="#"></a>
-      </li>
+<hr>
+</div>
+<div class="container">
+    <div class="row">
+      <div class="col-md-8 col-sm-6 col-xs-12">
+        <p class="copyright-text">Copyright &copy; 2019 All Rights Reserved by 
+           <a href="#">Aajkataxgyan.com</a>.
+       </p>
+   </div>
+
+   <div class="col-md-4 col-sm-6 col-xs-12">
+    <ul class="social-icons">
+      <li><a class="facebook" href="#"><i class="fa fa-facebook"></i></a></li>
+      <li><a class="twitter" href="#"><i class="fa fa-twitter"></i></a></li>
+      <li><a class="dribbble" href="#"><i class="fa fa-dribbble"></i></a></li>
+      <li><a class="linkedin" href="#"><i class="fa fa-linkedin"></i></a></li>   
   </ul>
 </div>
-</div>
-<div class="col-md-3">
-    <h4>Follow Us</h4>
-    <ul class="social-network social-circle">
-       <li><a href="#" class="icoFacebook" title="Facebook"><i class="fa fa-facebook"></i></a></li>
-       <li><a href="#" class="icoLinkedin" title="Linkedin"><i class="fa fa-linkedin"></i></a></li>
-   </ul>               
-</div>
-</div>
-<div class="row">
-    <div class="col-md-12 copy">
-        <p class="text-center">&copy; Copyright 2018 - Company Name.  All rights reserved.</p>
-    </div>
-</div>
-
-
 </div>
 </div>
 </footer>
